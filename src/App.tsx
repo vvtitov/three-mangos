c5e363eeb6b8cacf5d48e3cd6f0621b7ba4c4bee
Vladislav <vladislavtitov.r@gmail.com>
2025-05-13 00:11:30 -0300

    Clean instalation and first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+    <main id="main" className="w-full min-h-screen text-center flex items-center justify-center">
+      <p className="text-4xl font-bold">Vite + React + TS + TailwindCSS + ShadcnUI 👌</p>
+    </main>
+    </>
+  )
+}
+
+export default App
